e962ab9820681f2d40828b11363661377b15c4e4
Maximiliano Feldman <maxifeldman@hotmail.com>
2018-10-03 17:09:33 -0300

    Primer Commit

---
+++ pruebas/lexico.mk
@@ -0,0 +1,4 @@
+inicio
+    leer (abc, a23);
+    a23 == abc; // Lo que tira no tiene sentido, deberia de ser error lexico
+fin
---
+++ pruebas/sintactico.mk
@@ -0,0 +1,5 @@
+inicio
+    leer (abc, a23);
+    a23 := abc;
+		escribir(abc, a23)
+fin
